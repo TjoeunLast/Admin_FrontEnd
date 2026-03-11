b1f03aceb2624cc362c4fde264535684a03d2e42
lealle <84366652+lealle@users.noreply.github.com>
2026-03-12 02:42:12 +0900

    asd

--- barotruck_admin_web/next.config.ts
+++ barotruck_admin_web/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',  // <--- 이 줄을 반드시 추가해주세요!
+  async rewrites() {
+    return [
+      {
+        // 프론트에서 /api/... 로 요청을 보내면
+        source: '/api/:path*',
+        // Next.js 서버가 몰래 백엔드(HTTP)로 대신 전달해줌!
+        destination: 'http://barotruck.store:8080/api/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
