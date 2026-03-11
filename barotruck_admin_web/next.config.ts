9a48748523d5134f76543da1b472813a86cd512c
lealle <84366652+lealle@users.noreply.github.com>
2026-03-12 02:06:06 +0900

    Update next.config.ts

--- barotruck_admin_web/next.config.ts
+++ barotruck_admin_web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // <--- 이 줄을 반드시 추가해주세요!
+  // output: 'export',  // <--- 이 줄을 반드시 추가해주세요!
 };
 
 export default nextConfig;
